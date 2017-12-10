65e705f92d24022423435b8bb73889a7fe89b962
trsrreddy <rajareddy.t@gmail.com>
2017-12-10 06:19:25 +0530

    sample.java was updated with Java Code and text1.txt is created newly.

--- sample.java
+++ sample.java
@@ -1,10 +1,9 @@
- Volume in drive C has no label.
- Volume Serial Number is A277-3724
+/* HelloWorld.java
+ */
 
- Directory of C:\MyProgramming\git_local\git_sample
-
-12/10/2017  05:50 AM    <DIR>          .
-12/10/2017  05:50 AM    <DIR>          ..
-12/10/2017  05:50 AM                 0 sample.java
-               1 File(s)              0 bytes
-               2 Dir(s)  192,031,694,848 bytes free
+public class sample
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
